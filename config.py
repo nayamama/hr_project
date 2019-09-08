b2e8c4dae00c20e60eeee63ba9a61c5a923d11b6
Qi Yang <nayamama@hotmail.com>
2019-09-07 20:21:01 -0400

    change config file to remove common config

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+'''
 class Config(object):
     """
     Common configurations
@@ -10,6 +11,7 @@ class Config(object):
 
     #def SQLALCHEMY_DATABASE_URI(self):
     #    return 'postgresql://stage_test:1234abcd@{}:5432/stage_db'.format(self.DB_SERVER)   
+'''
 
 class DevelopmentConfig(Config):
     """
@@ -17,6 +19,7 @@ class DevelopmentConfig(Config):
     """
 
     DEBUG = True
+    TESTING = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://stage_test:1234abcd@192.168.1.76:5432/stage_db'
 
@@ -27,14 +30,17 @@ class ProductionConfig(Config):
     """
 
     DEBUG = False
+    TESTING = False
+    #DB_SERVER = 'postgres_host'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://stage_test:1234abcd@postgres_host:5432/stage_db'
 
 class TestingConfig(Config):
     """
     testing configurations
     """
     TESTING = True
-    DB_SERVER = '192.168.1.76'
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://stage_test:1234abcd@192.168.1.76:5432/stage_db'
 
 app_config = {
     'development': DevelopmentConfig,
